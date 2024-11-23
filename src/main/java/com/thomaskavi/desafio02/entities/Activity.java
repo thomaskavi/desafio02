9bbfae5476e6f8c287ae3b90701a219b2fe9ed8a
thomaskavi <thomaskavi2000@gmail.com>
2024-11-23 19:43:44 -0300

    feat: add Activity and Category entities with ManyToOne relationship and H2 table generation

---
+++ src/main/java/com/thomaskavi/desafio02/entities/Activity.java
@@ -0,0 +1,35 @@
+package com.thomaskavi.desafio02.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+
+@Entity
+@Table(name = "tb_activity")
+public class Activity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  private String description;
+
+  @Column(nullable = false)
+  private Double price;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+}
---
+++ src/main/java/com/thomaskavi/desafio02/entities/Category.java
@@ -0,0 +1,76 @@
+package com.thomaskavi.desafio02.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String description;
+
+  @OneToMany(mappedBy = "category")
+  private List<Activity> activities = new ArrayList<>();
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<Activity> getActivities() {
+    return activities;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Category other = (Category) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
