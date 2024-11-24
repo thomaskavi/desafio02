8cc16e382dbfd358e11636eb629b2bab5c7eb070
thomaskavi <thomaskavi2000@gmail.com>
2024-11-24 11:13:52 -0300

    feat: add Participant and Activity entities ManyToMany relation

--- src/main/java/com/thomaskavi/desafio02/entities/Activity.java
+++ src/main/java/com/thomaskavi/desafio02/entities/Activity.java
@@ -1,20 +1,21 @@
 package com.thomaskavi.desafio02.entities;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
-
-@Data
 
 @Entity
 @Table(name = "tb_activity")
@@ -33,10 +34,136 @@ public class Activity {
   private Double price;
 
   @ManyToOne
-  @JoinColumn(name = "category_id")
+  @JoinColumn(name = "category_id", nullable = false)
   private Category category;
 
-  @OneToMany(mappedBy = "activity")
+  @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Block> blocks = new ArrayList<>();
 
+  @ManyToMany(mappedBy = "activities") // Referência ao campo "activities" de Participant
+  private Set<Participant> participants = new HashSet<>();
+
+  public Activity(Long id, String name, String description, Double price, Category category, List<Block> blocks) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.category = category;
+    this.blocks = blocks;
+  }
+
+  public Activity() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public List<Block> getBlocks() {
+    return blocks;
+  }
+
+  public void setBlocks(List<Block> blocks) {
+    this.blocks = blocks;
+  }
+
+  @Override
+  public String toString() {
+    return "Activity [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + ", category="
+        + category + ", blocks=" + blocks + ", getBlocks()=" + getBlocks() + ", getCategory()=" + getCategory()
+        + ", getDescription()=" + getDescription() + ", getId()=" + getId() + ", getName()=" + getName()
+        + ", getPrice()=" + getPrice() + ", hashCode()=" + hashCode() + ", getClass()=" + getClass() + ", toString()="
+        + super.toString() + "]";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Activity other = (Activity) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (description == null) {
+      if (other.description != null)
+        return false;
+    } else if (!description.equals(other.description))
+      return false;
+    if (price == null) {
+      if (other.price != null)
+        return false;
+    } else if (!price.equals(other.price))
+      return false;
+    if (category == null) {
+      if (other.category != null)
+        return false;
+    } else if (!category.equals(other.category))
+      return false;
+    if (blocks == null) {
+      if (other.blocks != null)
+        return false;
+    } else if (!blocks.equals(other.blocks))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((description == null) ? 0 : description.hashCode());
+    result = prime * result + ((price == null) ? 0 : price.hashCode());
+    result = prime * result + ((category == null) ? 0 : category.hashCode());
+    result = prime * result + ((blocks == null) ? 0 : blocks.hashCode());
+    return result;
+  }
 }
--- src/main/java/com/thomaskavi/desafio02/entities/Participant.java
+++ src/main/java/com/thomaskavi/desafio02/entities/Participant.java
@@ -1,15 +1,20 @@
 package com.thomaskavi.desafio02.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-
 @Entity
 @Table(name = "tb_participant")
 public class Participant {
@@ -21,6 +26,13 @@ public class Participant {
   @Column(nullable = false)
   private String name;
 
-  @Column(unique = true)
+  @Column(nullable = false, unique = true)
   private String email;
+
+  @ManyToMany
+  @JoinTable(name = "tb_participant_activity", // Nome da tabela de associação
+      joinColumns = @JoinColumn(name = "participant_id"), // Coluna que faz referência à entidade Participant
+      inverseJoinColumns = @JoinColumn(name = "activity_id") // Coluna que faz referência à entidade Activity
+  )
+  private Set<Activity> activities = new HashSet<>();
 }
